0ef319387d0b8e876a9c55ccb347005469f53d74
rodrigo <lrodrigoespinoza@gmail.com>
2023-10-02 17:12:48 -0300

    5 CAMBIO DE TEXTO  Y OTROS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,17 @@ export default function Home() {
     w-2/3
     pt-20
     ml-40
+    text-center
+    item-center
     text-7xl 
     font-extrabold 
     text-transparent 
     bg-clip-text 
     bg-gradient-to-r from-indigo-500 via-purple-500 to-orange-400 animate-gradient-x
-">KNOW HOW SOLUTIONS</h1></div>
+">KNOW HOW SOLUTIONS</h1>
+<h2 className="text-yellow-400 pt-44 text-center item-center">Data Science | Ethical Hacking  | Development  | Automatication Test </h2>
+</div>
+<a className=" h-50 pt-44 pb-0 text-center items-center" href="#">© 2023 know how solutions. All rights reserved.</a>
     </main>
   )
 }
