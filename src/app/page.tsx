5c3e08f9c72ed17307f4a9a10c56bfcc5d985dd6
rodrigo <lrodrigoespinoza@gmail.com>
2023-10-02 16:49:08 -0300

    cuarto commint style title with tailwind.css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,16 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div><h1>KNOW HOW SOLUTIONS</h1></div>
+      <div><h1 className="    h-50
+    w-2/3
+    pt-20
+    ml-40
+    text-7xl 
+    font-extrabold 
+    text-transparent 
+    bg-clip-text 
+    bg-gradient-to-r from-indigo-500 via-purple-500 to-orange-400 animate-gradient-x
+">KNOW HOW SOLUTIONS</h1></div>
     </main>
   )
 }
