da3465dd2aedf8266dbe70a9c4b2fd2aa52310c6
rodrigo <lrodrigoespinoza@gmail.com>
2023-11-21 15:37:37 -0300

    letra cambio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,9 @@ const Page: React.FC = () => {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
       <Image src={logoRebel} alt="Logo" className="centered-logo" />
-        <h2 className="text-yellow-400 pt-44 text-center item-center">
-        Development | CiberSeguridad | Data 
-        </h2>
+        <h1 className="text-yellow-400 pt-44 text-center item-center">
+        Development | Cybersecurity | Data 
+        </h1>
       </div>
       <a className="h-50 pt-44 pb-0 text-center items-center" href="#">
         © 2023 Rebel Tech Solutions. All rights reserved.
