8ace14875e0b46be6ebec71185ca575abe9673aa
rodrigo <lrodrigoespinoza@gmail.com>
2023-11-21 15:28:27 -0300

    Cambios

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,21 @@
-import Image from 'next/image'
+import logoRebel from './logo.png'
+import Image from 'next/image';
 
-export default function Home() {
+
+const Page: React.FC = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div><h1 className="    h-50
-    w-2/3
-    pt-20
-    ml-40
-    text-center
-    item-center
-    text-7xl 
-    font-extrabold 
-    text-transparent 
-    bg-clip-text 
-    bg-gradient-to-r from-indigo-500 via-purple-500 to-orange-400 animate-gradient-x
-">Rebel Tech Solutions</h1>
-<h2 className="text-yellow-400 pt-44 text-center item-center">Data Science | Ethical Hacking  | Development  | Automatication Test </h2>
-</div>
-<a className=" h-50 pt-44 pb-0 text-center items-center" href="#">© 2023 Rebel Tech Solutions. All rights reserved.</a>
+      <div>
+      <Image src={logoRebel} alt="Logo" className="centered-logo" />
+        <h2 className="text-yellow-400 pt-44 text-center item-center">
+        Development | CiberSeguridad | Data 
+        </h2>
+      </div>
+      <a className="h-50 pt-44 pb-0 text-center items-center" href="#">
+        © 2023 Rebel Tech Solutions. All rights reserved.
+      </a>
     </main>
-  )
-}
+  );
+};
+
+export default Page;
