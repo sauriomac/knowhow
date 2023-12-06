33d7f31d72e7c7094f5118f4d17b21d8b5390c96
rodrigo <lrodrigoespinoza@gmail.com>
2023-12-06 12:09:26 -0300

    cambios tamaño texto movil y logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,14 @@ import Image from 'next/image';
 
 const Page: React.FC = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-custom-linear">
-  <div>
-    <Image src={logoRebel} alt="Logo" className="centered-logo mt-36" />
-    <h1 className="text-yellow-400 pt-44 text-center item-center text-sm md:text-3xl">
+    <main className="flex min-h-screen flex-col items-center justify-between px-4 md:px-24 w-full bg-custom-linear">
+  <div className="w-full text-center pt-32">
+    <Image src={logoRebel} alt="Logo" className="mx-auto mt-8 md:mt-36 w-3/4 md:w-auto" />
+    <h1 className="text-yellow-400 pt-36 md:pt-44 text-base md:text-3xl w-full">
       Development | Data | Cybersecurity 
     </h1>
   </div>
-  <a className="h-50 pt-44 pb-0 text-center items-center text-white" href="#">
+  <a className="w-full pt-12 pb-12 md:pt-44  text-center text-white" href="#">
     © 2023 Rebel Tech Solutions. All rights reserved.
   </a>
 </main>
