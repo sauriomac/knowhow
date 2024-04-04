05d1e20bd92d194c681ee2b02e3ba337d07618d8
rodrigo espinoza <lrodrigoespinoza@gmail.com>
2024-04-04 17:41:36 -0300

    correcion de texto

--- src/pages/Sendmarc.tsx
+++ src/pages/Sendmarc.tsx
@@ -169,7 +169,7 @@ const Sendmarc: React.FC = () => {
                     protección integral para el ecosistema de correo electrónico de una organización, asegurando
                     autenticidad, integridad y confiabilidad en la comunicación por correo electrónico. Para obtener
                     más información sobre cómo estas soluciones pueden beneficiar a su negocio, visite la
-                    páginas web de <a className="text-[#EFCE40] font-bold"  href="https://www.xcitium.com/">Sendmarc</a>.
+                    página web de <a className="text-[#EFCE40] font-bold"  href="https://www.xcitium.com/">Sendmarc</a>.
                 </p>
             </div>
         </div>
