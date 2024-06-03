672ec1e7408c07859380122389c68ad1954c6bb6
rodrigo espinoza <lrodrigoespinoza@gmail.com>
2024-06-03 02:38:51 -0400

    update sendmail

--- netlify/functions/sendEmail.js
+++ netlify/functions/sendEmail.js
@@ -14,9 +14,6 @@ exports.handler = async function(event, context) {
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
-    },
-    tls: {
-      ciphers: 'SSLv3'
     }
   });
 
